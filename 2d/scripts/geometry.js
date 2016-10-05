c5f33f64c655266ec776e6d15dba68eb5bd8953c
soma_arc <somaarcr@gmail.com>
2016-10-05 14:25:22 +0900

    Split files

---
+++ 2d/scripts/geometry.js
@@ -0,0 +1,316 @@
+const ID_CIRCLE = 0;
+const ID_INFINITE_CIRCLE = 1;
+const ID_TRANSFORM_BY_CIRCLES = 2;
+
+const CIRCLE_BODY = 0;
+const CIRCLE_CIRCUMFERENCE = 1;
+const CIRCLE_MOVE_MODE_NORMAL = 0;
+const CIRCLE_MOVE_MODE_NEAREST = 1;
+var Circle = function(x, y, r){
+    this.x = x;
+    this.y = y;
+    this.r = r;
+
+    this.circumferenceThickness = 10;
+    this.centerRadius = 10;
+
+    this.moveMode = CIRCLE_MOVE_MODE_NORMAL;
+}
+
+Circle.prototype = {
+    getPosition: function(){
+	return [this.x, this.y];
+    },
+    clone: function(){
+	return new Circle(this.x, this.y, this.r);
+    },
+    getUniformArray: function(){
+	return [this.x, this.y, this.r];
+    },
+    getUIParamArray: function(){
+        return [this.centerRadius,
+                this.circumferenceThickness];
+    },
+    move: function(componentId, mouse, diff){
+	if(componentId == CIRCLE_CIRCUMFERENCE){
+	    var dx = mouse[0] - this.x;
+	    var dy = mouse[1] - this.y;
+	    var dist = Math.sqrt((dx * dx) + (dy * dy));
+	    this.r = dist;
+	}else{
+	    this.x = mouse[0] - diff[0];
+	    this.y = mouse[1] - diff[1];
+            if(this.moveMode == CIRCLE_MOVE_MODE_NEAREST)
+                this.r = g_scene.getMinRadiusToOtherCircles(this);
+	}
+    },
+    removable: function(mouse, diff){
+	var dx = mouse[0] - this.x;
+	var dy = mouse[1] - this.y;
+	var dist = Math.sqrt((dx * dx) + (dy * dy));
+	return (dist < this.r);
+    },
+    // return [componentId,
+    //         difference between object position and mouse position]
+    selectable: function(mouse, scene){
+	var dx = mouse[0] - this.x;
+	var dy = mouse[1] - this.y;
+	var dist = Math.sqrt((dx * dx) + (dy * dy));
+        var distFromCircumference = dist - this.r;
+	if(distFromCircumference < 0 &&
+           Math.abs(distFromCircumference) < this.circumferenceThickness){
+	    return [CIRCLE_CIRCUMFERENCE, [dx, dy]];
+	}else if(dist < Math.abs(this.r - this.circumferenceThickness)){
+	    return [CIRCLE_BODY, [dx, dy]];
+	}
+	return [-1, [0, 0]];
+    }
+}
+
+const INFINITE_CIRCLE_CONTROL_POINT = 0;
+const INFINITE_CIRCLE_BODY = 1;
+const INFINITE_CIRCLE_ROTATION = 2;
+// Circle which have infinite radius.
+// It is defined by translation and rotation.
+// Initially it is reflection along the y-axis.
+var InfiniteCircle = function(x, y, thetaDegree){
+    this.x = x;
+    this.y = y;
+    this.thetaDegree = thetaDegree;
+    this.rotationMat2 = getRotationMat2(radians(thetaDegree));
+    this.invRotationMat2 = getRotationMat2(radians(-thetaDegree));
+
+    this.controlPointRadius = 10;
+    this.rotationControlCircleRadius = 50.;
+    this.rotationControlCircleThickness = 2;
+}
+
+InfiniteCircle.prototype = {
+    update: function(){
+	this.rotationMat2 = getRotationMat2(radians(this.thetaDegree));
+	this.invRotationMat2 = getRotationMat2(radians(-this.thetaDegree));
+    },
+    getPosition: function(){
+	return [this.x, this.y];
+    },
+    clone: function(){
+	return new InfiniteCircle(this.x, this.y, this.thetaDegree);
+    },
+    getUniformArray: function(){
+	return [this.x, this.y, this.thetaDegree];
+    },
+    getUIParamArray: function(){
+	return [this.controlPointRadius,
+		this.rotationControlCircleRadius,
+		this.rotationControlCircleThickness];
+    },
+    move: function(componentId, mouse, diff){
+	if(componentId == INFINITE_CIRCLE_CONTROL_POINT ||
+	   componentId == INFINITE_CIRCLE_BODY){
+	    this.x = mouse[0] - diff[0];
+	    this.y = mouse[1] - diff[1];
+	}else if(componentId == INFINITE_CIRCLE_ROTATION){
+	    var x = mouse[0] - this.x;
+	    var y = mouse[1] - this.y;
+	    this.thetaDegree = degrees(Math.atan2(-y, x) + Math.PI);
+	    this.update();
+	}
+    },
+    removable: function(mouse, diff){
+	var dx = mouse[0] - this.x;
+	var dy = mouse[1] - this.y;
+	var dist = Math.sqrt((dx * dx) + (dy * dy));
+	return (dist < this.controlPointRadius);
+    },
+    // return [componentId,
+    //         difference between object position and mouse position]
+    selectable: function(mouse, scene){
+	var dx = mouse[0] - this.x;
+	var dy = mouse[1] - this.y;
+	var dist = Math.sqrt((dx * dx) + (dy * dy));
+	if(dist < this.controlPointRadius){
+	    return [INFINITE_CIRCLE_CONTROL_POINT, [dx, dy]];
+	}
+	var p = vec2Diff(mouse, this.getPosition());
+	var rot = applyMat2(this.rotationMat2, p);
+	if(rot[0] > 0){
+	    return [INFINITE_CIRCLE_BODY, p];
+	}
+
+	p = vec2Sum(p, applyMat2(this.invRotationMat2, [this.rotationControlCircleRadius, 0]));
+	if(vec2Len(p) < this.controlPointRadius){
+	    return [INFINITE_CIRCLE_ROTATION, p];
+	}
+	
+	return [-1, [0, 0]];
+    }
+}
+
+const TRANSFORM_BY_CIRCLES_INNER_BODY = 0;
+const TRANSFORM_BY_CIRCLES_INNER_CIRCUMFERENCE = 1;
+const TRANSFORM_BY_CIRCLES_OUTER_BODY = 2;
+const TRANSFORM_BY_CIRCLES_OUTER_CIRCUMFERENCE = 3;
+
+var TransformByCircles = function(innerCircle, outerCircle){
+    this.inner = innerCircle;
+    this.outer = outerCircle;
+    this.inverted = circleInvert(this.inner, this.outer);
+}
+
+TransformByCircles.prototype = {
+    update: function(){
+        this.inverted = circleInvert(this.inner, this.outer);
+    },
+    getUniformArray: function(){
+	return this.inner.getUniformArray().concat(this.outer.getUniformArray(),
+						   this.inverted.getUniformArray());
+    },
+    clone: function(){
+        return new TransformByCircles(this.inner, this.outer);
+    },
+    move: function(componentId, mouse, diff){
+        var prevOuterX = this.outer.x; 
+        var prevOuterY = this.outer.y;
+        switch (componentId) {
+        case TRANSFORM_BY_CIRCLES_OUTER_BODY:
+            this.outer.x = mouse[0] - diff[0];
+	    this.outer.y = mouse[1] - diff[1];
+            this.inner.x += this.outer.x - prevOuterX;
+            this.inner.y += this.outer.y - prevOuterY;
+            break;
+        case TRANSFORM_BY_CIRCLES_OUTER_CIRCUMFERENCE:
+            var dx = mouse[0] - this.outer.x;
+	    var dy = mouse[1] - this.outer.y;
+	    var dist = Math.sqrt((dx * dx) + (dy * dy));
+	    this.outer.r = dist;
+            break;
+        case TRANSFORM_BY_CIRCLES_INNER_BODY:
+            var np = vec2Diff(mouse, diff);
+            var d = vec2Len(vec2Diff(this.outer.getPosition(), np));
+            if(d <= this.outer.r - this.inner.r){
+                this.inner.x = np[0];
+                this.inner.y = np[1];
+            }else{
+                diff[0] = mouse[0] - this.inner.x;
+                diff[1] = mouse[1] - this.inner.y;
+            }
+            break;
+        case TRANSFORM_BY_CIRCLES_INNER_CIRCUMFERENCE:
+            var dx = mouse[0] - this.inner.x;
+	    var dy = mouse[1] - this.inner.y;
+	    var nr = Math.sqrt((dx * dx) + (dy * dy));
+            var d = vec2Len(vec2Diff(this.outer.getPosition(), this.inner.getPosition()));
+            if(d <= this.outer.r - nr){
+                this.inner.r = nr;
+            }else{
+                diff[0] = mouse[0] - this.inner.x;
+                diff[1] = mouse[1] - this.inner.y;
+            }
+            break;
+        }
+        this.update();
+    },
+    removable: function(mouse, diff){
+        return this.outer.removable(mouse, diff);
+    },
+    // return [componentId,
+    //         difference between object position and mouse position]
+    selectable: function(mouse, scene){
+        var [componentId, diff] = this.inner.selectable(mouse, scene);
+        if(componentId == CIRCLE_BODY){
+            return [TRANSFORM_BY_CIRCLES_INNER_BODY, diff];
+        }else if(componentId == CIRCLE_CIRCUMFERENCE){
+            return [TRANSFORM_BY_CIRCLES_INNER_CIRCUMFERENCE, diff];
+        }
+        [componentId, diff] = this.outer.selectable(mouse, scene);
+        if(componentId == CIRCLE_BODY){
+            return [TRANSFORM_BY_CIRCLES_OUTER_BODY, diff];
+        }else if(componentId == CIRCLE_CIRCUMFERENCE){
+            return [TRANSFORM_BY_CIRCLES_OUTER_CIRCUMFERENCE, diff];
+        }
+	return [-1, [0, 0]];
+    },
+}
+
+var Scene = function(){
+    this.circles = [];
+    this.infiniteCircles =  [];
+    this.transformByCircles = [];
+    this.objects = {}
+    this.objects[ID_CIRCLE] = this.circles;
+    this.objects[ID_INFINITE_CIRCLE] = this.infiniteCircles;
+    this.objects[ID_TRANSFORM_BY_CIRCLES] = this.transformByCircles;
+}
+
+Scene.prototype = {
+    loadParameter: function(param){
+        this.circles = this.clone(param["circles"]);
+        this.infiniteCircles = this.clone(param["infiniteCircles"]);
+        this.transformByCircles = this.clone(param["transformByCircles"]);
+        this.objects[ID_CIRCLE] = this.circles;
+        this.objects[ID_INFINITE_CIRCLE] = this.infiniteCircles;
+        this.objects[ID_TRANSFORM_BY_CIRCLES] = this.transformByCircles;
+    },
+    clone: function(objects){
+	var obj = [];
+	for(var i = 0 ; i < objects.length ; i++){
+	    obj.push(objects[i].clone());
+	}
+	return obj;
+    },
+    getMinRadiusToOtherCircles: function(c){
+        var minRad = Number.MAX_VALUE;
+        for(var i = 0 ; i < this.circles.length ; i++){
+            var oc = this.circles[i];
+            if(c == oc) continue;
+            var nr = vec2Len(vec2Diff(c.getPosition(), oc.getPosition())) - oc.r;
+            if(nr < minRad){
+                minRad = nr;
+            }
+        }
+        return minRad
+    },
+    getNumCircles: function(){
+	return this.circles.length;
+    },
+    getNumInfiniteCircles: function(){
+	return this.infiniteCircles.length;
+    },
+    getNumTransformByCircles: function(){
+	return this.transformByCircles.length;
+    },
+    addCircle: function(canvas, mouse){
+	this.circles.push(new Circle(mouse[0], mouse[1], 100));
+	updateShaders(canvas);
+    },
+    // return [objectId, objectIndex, objectComponentId,
+    //         difference between object position and mouse position]
+    getSelectedObject: function(mouse){
+	for(objectId in Object.keys(this.objects)){
+	    objectId = parseInt(objectId);
+	    var objArray = this.objects[objectId];
+	    for(var i = 0 ; i < objArray.length ; i++){
+		var [componentId, diff] = objArray[i].selectable(mouse, this);
+		if(componentId != -1)
+		    return [objectId, i, componentId, diff];
+	    }
+	}
+	return [-1, -1, -1, [0, 0]];
+    },
+    move: function(id, index, componentId, mouse, diff){
+	if(id == -1) return;
+	var obj = this.objects[id][index];
+	if(obj != undefined)
+	    obj.move(componentId, mouse, diff);
+    },
+    remove: function(id, index, mouse, diff){
+	if(id == -1) return;
+	var objArray = this.objects[id];
+	var obj = objArray[index];
+	if(objArray != undefined &&
+	   objArray.length != 0 &&
+	   obj.removable(mouse, diff)){
+	    objArray.splice(index, 1);
+	}
+    }
+}
---
+++ 3d/scripts/geometry.js
@@ -0,0 +1,331 @@
+const ID_SCHOTTKY_SPHERE = 0;
+const ID_BASE_SPHERE = 1;
+const ID_TRANSFORM_BY_PLANES = 2;
+const ID_TRANSFORM_BY_SPHERES = 3;
+const ID_COMPOUND_PARABOLIC  = 4;
+
+var Sphere = function(x, y, z, r){
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.r = r;
+}
+
+Sphere.prototype = {
+    set : function(axis, val){
+	if(axis == 0){
+	    this.x = val;
+	}else if(axis == 1){
+	    this.y = val;
+	}else if(axis == 2){
+	    this.z = val;
+	}else if(axis == 3){
+	    this.r = val;
+	}
+    },
+    get : function(axis){
+	if(axis == 0){
+	    return this.x;
+	}else if(axis == 1){
+	    return this.y;
+	}else if(axis == 2){
+	    return this.z;
+	}else if(axis == 3){
+	    return this.r;
+	}
+    },
+    getPosition: function(){
+	return [this.x, this.y, this.z];
+    },
+    getUniformArray: function(){
+	return [this.x, this.y, this.z, this.r];
+    },
+    clone: function(){
+	return new Sphere(this.x, this.y, this.z, this.r);
+    },
+    // dx and dy are distance between preveous mouse position and current mouse position.
+    // Move this sphere along the selected axis.
+    move: function(dx, dy, axis, prevObject, schottkyCanvas){
+	var v = schottkyCanvas.axisVecOnScreen[axis];
+	var lengthOnAxis = v[0] * dx + v[1] * dy;
+	var p = calcCoordOnAxis(schottkyCanvas.camera,
+				schottkyCanvas.canvas.width,
+				schottkyCanvas.canvas.height,
+				axis, v, prevObject.getPosition(),
+				lengthOnAxis);
+	this.set(axis, p[axis]);
+    },
+    setRadius: function(mx, my, dx, dy, prevObject, schottkyCanvas){
+	//We assume that prevObject is Sphere.
+	var spherePosOnScreen = calcPointOnScreen(prevObject.getPosition(),
+						  schottkyCanvas.camera,
+						  schottkyCanvas.canvas.width,
+						  schottkyCanvas.canvas.height);
+	var diffSphereAndPrevMouse = [spherePosOnScreen[0] - schottkyCanvas.prevMousePos[0],
+				      spherePosOnScreen[1] - schottkyCanvas.prevMousePos[1]];
+	var r = Math.sqrt(diffSphereAndPrevMouse[0] * diffSphereAndPrevMouse[0] +
+			  diffSphereAndPrevMouse[1] * diffSphereAndPrevMouse[1]);
+	var diffSphereAndMouse = [spherePosOnScreen[0] - mx,
+				  spherePosOnScreen[1] - my];
+	var distToMouse = Math.sqrt(diffSphereAndMouse[0] * diffSphereAndMouse[0] +
+				    diffSphereAndMouse[1] * diffSphereAndMouse[1]);
+	var d = distToMouse - r;
+	
+	//TODO: calculate tangent sphere
+	this.r = prevObject.r + d * 3;
+    },
+    getComponentFromId(id){
+	return this;
+    }
+}
+
+// Initially planes are aligned along the z-axis
+// Rotation is defined by theta and phi
+// center is (0, 0, 0)
+var ParabolicTransformation = function(){
+    this.distToP1 = -300;
+    this.distToP2 = 300;
+    this.theta = 0; // Degree
+    this.phi = 0; // Degree
+    this.rotationMat3 = getIdentityMat3();
+    this.invRotationMat3 = getIdentityMat3();
+    this.size = 1200;
+    this.twist = 0.; // Degree
+    this.twistMat3 = getIdentityMat3();
+    this.invTwistMat3 = getIdentityMat3();
+    this.update();
+}
+
+ParabolicTransformation.prototype = {
+    clone: function(){
+	var obj = new ParabolicTransformation();
+	obj.distToP1 = this.distToP1;
+	obj.distToP2 = this.distToP2;
+	obj.theta = this.theta;
+	obj.phi = this.phi;
+	obj.size = this.size;
+	obj.twist = this.twist;
+	obj.update();
+	return obj;
+    },
+    getUniformArray: function(){
+	return [this.distToP1, this.distToP2, this.size,
+		radians(this.theta), radians(this.phi), this.twist];
+    },
+    getComponentFromId: function(id){
+	return this;
+    },
+    update: function(){
+	var rotateX = getRotationXAxis(radians(this.theta));
+	var rotateY = getRotationYAxis(radians(this.phi));
+	this.rotationMat3 = prodMat3(rotateX, rotateY);
+	rotateX = getRotationXAxis(radians(-this.theta));
+	rotateY = getRotationYAxis(radians(-this.phi));
+	this.invRotationMat3 = prodMat3(rotateY, rotateX);
+	this.twistMat3 = getRotationZAxis(radians(this.twist));
+	this.invTwistMat3 = getRotationZAxis(radians(-this.twist));
+    }
+}
+
+// Currently, we aligne the transformation along the z-axis only.
+var CompoundParabolic = function(){
+    // innerSphere and outer sphere is kissing
+    this.inner = new Sphere(0, 0, 1000, 500);// componentId = 0
+    this.outer = new Sphere(0, 0, 900, 600);// componentId = 1
+    this.inverted = sphereInvert(this.inner, this.outer); // componentId = 2
+    this.theta = 45; //Degree
+    this.rotationMat3 = getRotationZAxis(radians(this.theta));
+    this.invRotationMat3 = getRotationZAxis(radians(-this.theta));
+}
+
+CompoundParabolic.prototype = {
+    getUniformArray: function(){
+	return this.inner.getUniformArray().concat(this.outer.getUniformArray(),
+						   this.inverted.getUniformArray());
+    },
+    clone: function(){
+	var obj = new CompoundParabolic();
+	obj.inner = this.inner.clone();
+	obj.outer = this.outer.clone();
+	obj.inverted = this.inverted.clone();
+	obj.update();
+	return obj;
+    },
+    update: function(){
+	this.inverted = sphereInvert(this.inner, this.outer);
+	this.rotationMat3 = getRotationZAxis(radians(this.theta));
+	this.invRotationMat3 = getRotationZAxis(radians(-this.theta));
+    },
+    move: function(dx, dy, axis, prevObject, schottkyCanvas){
+	var d = prevObject.inner.get(axis)- prevObject.outer.get(axis);
+	this.outer.move(dx, dy, axis, prevObject.outer, schottkyCanvas);
+	// Keep spheres kissing
+	this.inner.set(axis, this.outer.get(axis) + d);
+	this.update();
+    },
+    setRadius: function(mx, my, dx, dy, prevObject, schottkyCanvas){
+	this.outer.setRadius(mx, my, dx, dy, prevObject.outer, schottkyCanvas);
+	// Keep spheres kissing
+	this.inner.set(2, prevObject.inner.get(2) + this.outer.r - prevObject.outer.r);
+	this.update();
+    },
+    getComponentFromId: function(id){
+	if(id == 0){
+	    return this.inner;
+	}else if(id == 1){
+	    return this.outer;
+	}else if(id == 2){
+	    return this.inverted;
+	}
+    }
+}
+
+// Transformation defined by two spheres
+var TransformBySpheres = function(){
+    // innerSphere and outer sphere is kissing
+    this.inner = new Sphere(0, 0, 1000, 500);// componentId = 0
+    this.outer = new Sphere(0, 0, 900, 600);// componentId = 1
+    this.inverted = sphereInvert(this.inner, this.outer); // componentId = 2
+}
+
+TransformBySpheres.prototype = {
+    getUniformArray: function(){
+	return this.inner.getUniformArray().concat(this.outer.getUniformArray(),
+						   this.inverted.getUniformArray());
+    },
+    clone: function(){
+	var obj = new TransformBySpheres();
+	obj.inner = this.inner.clone();
+	obj.outer = this.outer.clone();
+	obj.inverted = this.inverted.clone();
+	obj.update();
+	return obj;
+    },
+    update: function(){
+	this.inverted = sphereInvert(this.inner, this.outer);
+    },
+    move: function(dx, dy, axis, prevObject, schottkyCanvas){
+	var d = prevObject.inner.get(axis)- prevObject.outer.get(axis);
+	this.outer.move(dx, dy, axis, prevObject.outer, schottkyCanvas);
+	// Keep spheres kissing
+	this.inner.set(axis, this.outer.get(axis) + d);
+	this.update();
+    },
+    setRadius: function(mx, my, dx, dy, prevObject, schottkyCanvas){
+	this.outer.setRadius(mx, my, dx, dy, prevObject.outer, schottkyCanvas);
+	// Keep spheres kissing
+	this.inner.set(2, prevObject.inner.get(2) + this.outer.r - prevObject.outer.r);
+	this.update();
+    },
+    getComponentFromId: function(id){
+	if(id == 0){
+	    return this.inner;
+	}else if(id == 1){
+	    return this.outer;
+	}else if(id == 2){
+	    return this.inverted;
+	}
+    }
+}
+
+var Camera = function(target, fovDegree, eyeDist, up){
+    this.target = target;
+    this.prevTarget = target;
+    this.fovDegree = fovDegree;
+    this.eyeDist = eyeDist;
+    this.up = up;
+    this.theta = 0;
+    this.phi = 0;
+    this.position;
+    this.update();
+}
+
+// Camera is on the sphere which its center is target and radius is eyeDist.
+// Position is defined by two angle, theta and phi.
+Camera.prototype = {    
+    update: function(){
+	this.position = [this.eyeDist * Math.cos(this.phi) * Math.cos(this.theta),
+			 this.eyeDist * Math.sin(this.phi),
+			 -this.eyeDist * Math.cos(this.phi) * Math.sin(this.theta)];
+	this.position = sum(this.target, this.position);
+	if(Math.abs(this.phi) % (2 * Math.PI) > Math.PI / 2. &&
+	   Math.abs(this.phi) % (2 * Math.PI) < 3 * Math.PI / 2.){
+	    this.up = [0, -1, 0];
+	}else{
+	    this.up = [0, 1, 0];
+	}
+    }
+}
+
+
+var Scene = function(){
+    this.schottkySpheres = [];
+    this.baseSpheres = [new Sphere(0, 0, 0, 125)];
+    this.transformByPlanes = [];
+    this.transformBySpheres = [];
+    this.compoundParabolic = [];
+}
+
+
+Scene.prototype = {
+    loadParameter: function(param){
+	this.schottkySpheres = this.clone(param["schottkySpheres"]);
+	this.baseSpheres = this.clone(param["baseSpheres"]);
+	this.transformByPlanes = this.clone(param["transformByPlanes"]);
+	this.transformBySpheres = this.clone(param["transformBySpheres"]);
+	this.compoundParabolic = this.clone(param["compoundParabolic"]);
+    },
+    clone: function(objects){
+	var obj = [];
+	for(var i = 0 ; i < objects.length ; i++){
+	    obj.push(objects[i].clone());
+	}
+	return obj;
+    },
+    addSchottkySphere: function(schottkyCanvas, orbitCanvas){
+	this.schottkySpheres.push(new Sphere(500, 500, 0, 300));
+	updateShaders(schottkyCanvas, orbitCanvas);
+    },
+    addBaseSphere: function(schottkyCanvas, orbitCanvas){
+	this.baseSpheres.push(new Sphere(500, 500, 0, 125));
+	updateShaders(schottkyCanvas, orbitCanvas);
+    },
+    removeSchottkySphere: function(schottkyCanvas, orbitCanvas, sphereIndex){
+	if(this.schottkySpheres.length == 0) return;
+	this.schottkySpheres.splice(sphereIndex, 1);
+	schottkyCanvas.selectedObjectId = -1;
+	schottkyCanvas.selectedObjectIndex = -1;
+	updateShaders(schottkyCanvas, orbitCanvas);
+    },
+    removeBaseSphere: function(schottkyCanvas, orbitCanvas, sphereIndex){
+	if(this.baseSpheres.length == 1) return;
+	this.baseSpheres.splice(sphereIndex, 1);
+	schottkyCanvas.selectedObjectId = -1;
+	schottkyCanvas.selectedObjectIndex = -1;
+	updateShaders(schottkyCanvas, orbitCanvas);
+    },
+    getObjects: function(){
+	var obj = {};
+	obj[ID_SCHOTTKY_SPHERE] = this.schottkySpheres;
+	obj[ID_BASE_SPHERE] = this.baseSpheres;
+	obj[ID_TRANSFORM_BY_PLANES] = this.transformByPlanes;
+	obj[ID_TRANSFORM_BY_SPHERES] = this.transformBySpheres;
+	obj[ID_COMPOUND_PARABOLIC] = this.compoundParabolic;
+	return obj;
+    },
+    getNumSchottkySpheres: function(){
+	return this.schottkySpheres.length;
+    },
+    getNumBaseSpheres: function(){
+	return this.baseSpheres.length;
+    },
+    getNumTransformByPlanes: function(){
+	return this.transformByPlanes.length;
+    },
+    getNumTransformBySpheres: function(){
+	return this.transformBySpheres.length;
+    },
+    getNumCompoundParabolic: function(){
+	return this.compoundParabolic.length;
+    }
+}
