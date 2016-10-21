91ebb9d4cb8fd44e145b5c58691d497f578c3108
soma_arc <somaarcr@gmail.com>
2016-10-21 10:59:17 +0900

    Use drawArrays

--- 2d/scripts/main.js
+++ 2d/scripts/main.js
@@ -224,29 +224,23 @@ function setupSchottkyProgram(scene, renderCanvas){
         uniLocation[n++] = gl.getUniformLocation(program, 'u_twistedLoxodromicUIParam'+ i);
     }
     
-    var position = [-1.0, 1.0, 0.0,
-                    1.0, 1.0, 0.0,
-	            -1.0, -1.0,  0.0,
-	            1.0, -1.0, 0.0
-                   ];
-    var index = [
-	0, 2, 1,
-	1, 2, 3
+    var vertex = [
+            -1, -1,
+            -1, 1,
+             1, -1,
+             1, 1
     ];
-    var vPosition = createVbo(gl, position);
-    var vIndex = createIbo(gl, index);
-    var vAttLocation = gl.getAttribLocation(program, 'position');
+    var vPosition = createVbo(gl, vertex);
+    var vAttLocation = gl.getAttribLocation(program, 'a_vertex');
     gl.bindBuffer(gl.ARRAY_BUFFER, vPosition);
     gl.enableVertexAttribArray(vAttLocation);
-    gl.vertexAttribPointer(vAttLocation, 3, gl.FLOAT, false, 0, 0);
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, vIndex);
+    gl.vertexAttribPointer(vAttLocation, 2, gl.FLOAT, false, 0, 0);
 
     var switchProgram = function(){
         gl.useProgram(program);
         gl.bindBuffer(gl.ARRAY_BUFFER, vPosition);
         gl.enableVertexAttribArray(vAttLocation);
-        gl.vertexAttribPointer(vAttLocation, 3, gl.FLOAT, false, 0, 0);
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, vIndex);
+        gl.vertexAttribPointer(vAttLocation, 2, gl.FLOAT, false, 0, 0);
     }
 
     var render = function(elapsedTime){
@@ -292,7 +286,7 @@ function setupSchottkyProgram(scene, renderCanvas){
 				scene.objects[ID_TWISTED_LOXODROMIC][i].invRotationMat2);
             gl.uniform2fv(uniLocation[uniI++], scene.objects[ID_TWISTED_LOXODROMIC][i].getUIParamArray());
 	}
-        gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
+        gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 
 	gl.flush();
     }
