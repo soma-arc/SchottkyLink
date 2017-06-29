e6abf1d550d058ed20c94086d29ba4f60223746b
soma_arc <somaarcr@gmail.com>
2017-06-29 22:22:57 +0900

    Add control panel component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+import Vue from 'vue';
 import Canvas2D from './canvas2d';
 import Scene from './geometry/scene.js';
+import ControlPanel from './vue/controlPanel.vue';
 
 const PRESET = require('./preset.json');
 
@@ -8,4 +10,14 @@ window.addEventListener('load', () => {
     scene.load(PRESET);
     const canvas2d = new Canvas2D('canvas', scene);
     canvas2d.render();
+
+    const d = { 'scene': scene };
+    new Vue({
+        el: '#controlPanel',
+        data: d,
+        render: (h) => {
+            return h('control-panel', { 'props': d })
+        },
+        components: { 'control-panel': ControlPanel }
+    })
 });
---
+++ src/vue/controlPanel.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <select size="5" v-model="selected">
+      <template v-for="objs in scene.objects" v-bind:objs="objs">
+        <option v-for="obj in objs" v-bind:value="obj">
+          {{ obj.id }}
+        </option>
+      </template>
+    </select>
+  </div>
+</template>
+
+<script>
+  export default {
+      props: ['scene'],
+  }
+</script>
