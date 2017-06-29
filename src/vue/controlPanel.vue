94c453fd3976941150ab8b554027c8a7c2b0b785
soma_arc <somaarcr@gmail.com>
2017-06-30 00:43:00 +0900

    Use selected object data

--- src/vue/controlPanel.vue
+++ src/vue/controlPanel.vue
@@ -1,17 +1,23 @@
 <template>
-  <div>
-    <select size="5" v-model="selected">
-      <template v-for="objs in scene.objects" v-bind:objs="objs">
-        <option v-for="obj in objs" v-bind:value="obj">
-          {{ obj.id }}
-        </option>
-      </template>
-    </select>
-  </div>
+    <div>
+        <select size="5" v-model="selected">
+            <template v-for="objs in scene.objects" v-bind:objs="objs">
+                <option v-for="obj in objs" v-bind:value="obj">
+                    {{ obj.id }}
+                </option>
+            </template>
+        </select>
+        <div>{{ selected.id }}</div>
+    </div>
 </template>
 
 <script>
   export default {
       props: ['scene'],
+      data: function () {
+          return {
+              selected: { id:0 }
+          }
+      }
   }
 </script>
