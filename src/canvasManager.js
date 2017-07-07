c8b11009b01c7fc7a7d135dd875bd6d1e67d0f27
soma_arc <somaarcr@gmail.com>
2017-07-07 17:12:24 +0900

    Add canvas manager

---
+++ src/canvasManager.js
@@ -0,0 +1,34 @@
+import Scene2D from './geometry/scene.js';
+import Canvas2D from './canvas2d.js';
+
+const RENDER_2D = 0;
+const RENDER_3D = 1;
+
+export default class canvasManager {
+    constructor() {
+        this.scene2d = new Scene2D();
+        this.canvas2d = {};
+
+        this.scene3d = {};
+        this.canvas3dGen = {};
+        this.canvas3dOrb = {};
+
+        this.mode = RENDER_2D;
+
+        this.resizeCallback = this.resize.bind(this);
+    }
+
+    init() {
+        // canvas should be initialize after initializing Vue
+        this.canvas2d = new Canvas2D('canvas', this.scene2d);
+        this.canvas2d.render();
+    }
+
+    resize() {
+        if (this.mode === RENDER_2D) {
+            this.canvas2d.resizeCanvas();
+            this.canvas2d.compileRenderShader();
+            this.canvas2d.render();
+        }
+    }
+}
--- src/index.js
+++ src/index.js
@@ -6,18 +6,18 @@ import TextureHandler from './textureHandler.js';
 import Root from './vue/root.vue';
 import 'keen-ui/src/bootstrap';
 
+import CanvasManager from './canvasManager.js';
+
 window.addEventListener('load', () => {
     // load default textures
     const texLoad = TextureHandler.init();
-
     Promise.all(texLoad).then(function() {
-        const scene = new Scene();
+        const canvasManager = new CanvasManager();
 
-        const d = { 'scene': scene,
-                    'canvas2d': {} };
+        const d = { 'canvasManager': canvasManager };
 
         /* eslint-disable no-new */
-        new Vue({
+        const app = new Vue({
             el: '#app',
             data: d,
             render: (h) => {
@@ -25,35 +25,17 @@ window.addEventListener('load', () => {
             },
             components: { 'root': Root }
         })
-        d.canvas2d = new Canvas2D('canvas', scene);
-        const canvas2d = d.canvas2d;
-        canvas2d.render();
 
-        function resized() {
-            canvas2d.resizeCanvas();
-            canvas2d.compileRenderShader();
-            canvas2d.render();
-        }
+        canvasManager.init();
 
-        let resizeTimer = setTimeout(resized, 500);
+        let resizeTimer = setTimeout(canvasManager.resizeCallback, 500);
         window.addEventListener('resize', () => {
             window.clearTimeout(resizeTimer);
-            resizeTimer = window.setTimeout(resized, 500);
+            resizeTimer = window.setTimeout(canvasManager.resizeCallback, 500);
         })
 
-        window.addEventListener('keydown', function(event) {
-            switch (event.key) {
-            case '0':
-                scene.loadPreset(0);
-                canvas2d.compileRenderShader();
-                canvas2d.render();
-                break;
-            case '1':
-                scene.loadPreset(1);
-                canvas2d.compileRenderShader();
-                canvas2d.render();
-                break;
-            }
-        });
+        window.addEventListener('popstate', () => {
+            app.currentRoute = window.location.pathname
+        })
     });
 });
