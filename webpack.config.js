edf3937db69528cbcc9b26bf1dfe3353ed999dfb
soma_arc <somaarcr@gmail.com>
2017-07-16 22:22:26 +0900

    Load keen-ui with babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = () => ({
             },
             {
                 test: /\.js$/,
-                exclude: /node_modules/,
+                exclude: /node_modules(?!(\/|\\)keen-ui)/,
                 loader: 'babel-loader',
             },
             {
